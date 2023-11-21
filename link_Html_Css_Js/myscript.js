b045c5a8230db1ca19b941d4a7afb70240cb5160
Nitish Jha <nitishkumar0914@gmail.com>
2023-11-22 00:10:18 +0530

    buttons

--- link_Html_Css_Js/myscript.js
+++ link_Html_Css_Js/myscript.js
@@ -1,3 +1,4 @@
+// prompt , alert and confirm in js and write text in DOM under body tag
 console.log("script link succesfully");
 let names = prompt("enter your Name: ");
 let a = confirm("do you want to write in Dom"); //return true or false
@@ -6,3 +7,11 @@ if (a){
     document.write(names)   //to write data under body tag
 }
 alert(`Welcome ${names} `);
+
+//get dom element data throuh innerHTML and outerHTML
+let b = first.innerHTML
+let c = first.outerHTML
+console.log(b,c);
+
+// change innetHTML
+first.innerHTML = "heading changed by js through innerhtml properties"
